dd9d3d29c260414014898b890bb3630011431884
Derrick W. Turk <dwt@terminusdatascience.com>
2022-06-11 13:10:14 -0500

    ok, that's... nice

--- rwo/cli/md5hash/md5hash.ml
+++ rwo/cli/md5hash/md5hash.ml
@@ -1,11 +1,16 @@
 open Core
 
-let do_hash file = Md5.digest_file_blocking file |> Md5.to_hex |> print_endline
+let do_hash hash_length file =
+  Md5.digest_file_blocking file
+  |> Md5.to_hex
+  |> (fun s -> String.prefix s hash_length)
+  |> print_endline
 
 let command = Command.basic
   ~summary:"Compute  the MD5 hash of a file"
   ~readme:(fun () -> "LOL")
-  Command.Param.(map (anon ("filename" %: string))
-    ~f:(fun filename () -> do_hash filename))
+  (let%map_open.Command hash_length = anon ("hash_length" %: int)
+   and filename = anon ("filename" %: Filename_unix.arg_type) in
+   fun () -> do_hash hash_length filename)
 
 let () = Command_unix.run ~version:"0.1" ~build_info:"LOL" command
