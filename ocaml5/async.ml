7be590fffcb2f853cd4c9cfcd7cc26864beebb73
Derrick W. Turk <dwt@terminusdatascience.com>
2022-07-23 12:03:35 -0500

    so far so good

--- ocaml5/async.ml
+++ ocaml5/async.ml
@@ -3,12 +3,13 @@ open Effect.Deep
 
 type _ Effect.t +=
   | Yield: unit Effect.t
+  | Async: (unit -> unit) -> unit Effect.t
 
 let yield () = perform Yield
+let async f = perform (Async f)
 
-let run_async f =
+let rec run_async_on f q =
   let open Queue in
-  let q = create () in
   let defer k = add k q in 
   let next () = try take q () with Empty -> () in
   match_with f ()
@@ -20,12 +21,26 @@ let run_async f =
               defer (continue k);
               next ()
           end
+        | Async f -> Some begin
+            fun (k: (a, _) continuation) ->
+              defer (fun () -> run_async_on f q);
+              continue k ()
+          end
         | _ -> None
     }
+
+let run_async f =
+  let q = Queue.create () in
+  run_async_on f q
 
 let () =
   run_async begin fun () ->
     print_endline "first this";
+    async begin fun () ->
+      print_endline "followed by this";
+      yield ();
+      print_endline "followed by the other";
+    end;
     yield ();
     print_endline "then that"
   end
--- ocaml5/async.mli
+++ ocaml5/async.mli
@@ -1,2 +1,4 @@
 val yield: unit -> unit
+val async: (unit -> unit) -> unit
+val run_async_on: (unit -> unit) -> (unit -> unit) Queue.t -> unit
 val run_async: (unit -> unit) -> unit
